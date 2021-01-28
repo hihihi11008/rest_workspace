18cf850dda7a2be878a7a0b0f44b955a9c915f03
jieon <hihihi11008@gmail.com>
2021-01-28 18:13:30 +0900

    rest0128 커밋

---
+++ RestProject/src/main/java/com/koreait/restproject/android/ChatThread.java
@@ -0,0 +1,62 @@
+package com.koreait.restproject.android;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.net.Socket;
+
+//소켓을 가지고, 입려과 출력을 수행하되 다른객체와 독립적으로 수행될 수 있어야 하므로 
+//쓰레드로 정의한다 
+public class ChatThread extends Thread{
+	Socket socket;
+	ChatServer chatServer;
+	BufferedReader buffr;
+	BufferedWriter buffw;
+	boolean flag=true; //쓰레드 가동 여부를 결정하는 논리값, Thread를 죽이고 싶다면 false로 준다 
+	
+	public ChatThread(Socket socket, ChatServer chatServer) {
+		this.socket = socket;
+		this.chatServer = chatServer;
+		
+		try {
+			buffr = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+			buffw = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	//메시지 청취
+	public void listen() {
+		String msg = null;
+		try {
+			msg = buffr.readLine();
+			chatServer.area.append(msg+"\n");//로그에 남기기
+
+			for (ChatThread chatThread : chatServer.vec) {//접속한 모든 접속자에 대핸 send 호출 == broad Casting 방송하자! 
+				chatThread.send(msg);
+			}
+			send(msg);
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	//메시지 전송
+	public void send(String msg) {
+		try {
+			buffw.write(msg+"\n");
+			buffw.flush();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	public void run() {
+		while (flag) {
+			listen();
+		}
+	}
+}
